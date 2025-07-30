c4b788047123abdb1bdc204a9fd3942e7b61e430
shawkatimran <shawkatimran4@gmail.com>
2025-07-31 00:43:11 +0600

    Remove PyCache

--- pages/auth/LoginPage.py
+++ pages/auth/LoginPage.py
@@ -5,12 +5,19 @@ class LoginPage(BasePage):
     USERNAME_FIELD = (By.ID, "username")      
     PASSWORD_FIELD = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "loginBtn")
-    ERROR_MESSAGE = (By.CLASS_NAME, "error") 
+    ERROR_MESSAGE = (By.CLASS_NAME, "error")
+    LOGIN_BUTTON = (By.XPATH, "//span[@class='dlsd-btn-text' and text()='নাগরিক লগইন']")
+    SIGN_IN_BUTTON = (By.ID, "kc-login")
+    
+
+
 
     def login(self, username, password):
+        self.click(self.LOGIN_BUTTON)
         self.send_keys(self.USERNAME_FIELD, username)
         self.send_keys(self.PASSWORD_FIELD, password)
-        self.click(self.LOGIN_BUTTON)
+        self.click(self.SIGN_IN_BUTTON)
+        
 
     def get_error_message(self):
         return self.get_text(self.ERROR_MESSAGE)
--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -1,17 +1,28 @@
 from pages.auth.LoginPage import LoginPage
 from config import LOGIN_PAGE_URL
 from drivers import get_chrome_driver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
 
 def test_valid_login(driver):
-    driver.get("https://www.google.com")
+    driver.get("https://uat-slsd.mohpw.gov.bd/")
+
+    login_page = LoginPage(driver)
+    login_page.login("01961627162", "123456aA@")
+    time.sleep(5) 
+ 
     
 
-    #  stay for a while to see the page
-    driver.implicitly_wait(10)
-
-    # login_page.login("admin", "admin123")
+    
 
 
+   
+
+    
+    
 
 
 
